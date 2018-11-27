8626dc52cfb73873eaa5adeb54637e2ae300f824
unknown <esokoletsky@gmail.com>
2018-11-27 10:38:33 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://esokoletsky:deskjet11@ds133113.mlab.com:33113/workout_app' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
